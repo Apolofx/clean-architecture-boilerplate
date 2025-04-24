519066b1ee1b61f932105a9dfd5575ffc3f45c32
Nacho <nacho.conso@gmail.com>
2025-04-24 17:45:33 -0300

    ignore sqlite files

--- src/infrastructure/database/init-sqlite.ts
+++ src/infrastructure/database/init-sqlite.ts
@@ -1,11 +1,14 @@
 import { Database } from "sqlite3";
 
 export const initSQLite = (): Database => {
-  const db = new Database(":memory:");
+  const db = new Database("db.sqlite");
 
   db.serialize(() => {
     db.run("CREATE TABLE IF NOT EXISTS users (id TEXT PRIMARY KEY, name TEXT)");
-    db.run("INSERT INTO users (id, name) VALUES (?, ?)", ["1", "John Doe"]);
+    db.run(
+      "INSERT INTO users (id, name) VALUES (?, ?) ON CONFLICT(id) DO UPDATE SET name=excluded.name",
+      ["1", "John Doe"]
+    );
   });
 
   return db;
